ca7ddb23b4ae25dceae6ee01e94169dcb169cefd
ChenGit55 <chenpsilva@gmail.com>
2024-07-28 12:32:10 -0300

    Changes the font family.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import ContactUs from "@/components/ContactUs";
 
-const inter = Inter({ subsets: ["latin"] });
+const monserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "LA Evolution",
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={monserrat.className}>
         <Header />
         <main className="global-container">{children}</main>
         <ContactUs />
